0c1a66c56b34c1d782cfcadfb585e5f496f544b5
Arek <acieply97@gmail.com>
2023-11-28 20:36:32 +0100

    fix files

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
 set -e
-
-envsubs < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
